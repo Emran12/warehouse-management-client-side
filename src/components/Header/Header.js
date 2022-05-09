4c4369879032781ef18900daf3852c0dd5375b58
Emran12 <emran6452@gmail.com>
2022-05-09 07:24:59 +0600

    Navbar added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="/home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/products">Products</Nav.Link>
+            <Nav.Link href="/blog">Blog</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
