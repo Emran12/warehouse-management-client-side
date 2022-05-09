ee2075b43c83a8548943fe79fd2e26c1e0bc9ace
Emran12 <emran6452@gmail.com>
2022-05-09 12:18:04 +0600

    banner added

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Col, Row } from "react-bootstrap";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -9,36 +9,40 @@ const Products = () => {
       .then((data) => setProducts(data));
   }, []);
   return (
-    <div>
+    <div className="container">
       <h1>Products</h1>
-      {products.slice(0, 7).map((product) => (
-        <div key={product._id}>
-          <Card style={{ width: "20rem" }}>
-            <Card.Body>
-              <Card.Title>{product.productName}</Card.Title>
-              <Card.Img variant="top" src={product.img} />
-              <Card.Text>
-                {product.description.length > 200
-                  ? `${product.description.slice(0, 200)}...`
-                  : product.description}
-              </Card.Text>
-              <Card.Text>
-                <h5>
-                  Supplier Name: <smal>{product.supplierName}</smal>
-                </h5>
-                <h5>Price: ${product.price}</h5>
-              </Card.Text>
-              <Card.Text>
-                <h5>
-                  Availability:
-                  {product.quantity > 0 ? " in Stock" : "out of stock"}{" "}
-                </h5>
-              </Card.Text>
-              <Button variant="primary">Manage Product</Button>
-            </Card.Body>
-          </Card>
-        </div>
-      ))}
+      <Row xs={1} md={2} lg={3} className="g-4">
+        {products.slice(0, 6).map((product) => (
+          <div key={product._id}>
+            <Col>
+              <Card style={{ width: "20rem" }}>
+                <Card.Body>
+                  <Card.Title>{product.productName}</Card.Title>
+                  <Card.Img variant="top" height="300px" src={product.img} />
+                  <Card.Text>
+                    {product.description.length > 200
+                      ? `${product.description.slice(0, 200)}...`
+                      : product.description}
+                  </Card.Text>
+                  <Card.Text>
+                    <h5>
+                      Supplier Name: <smal>{product.supplierName}</smal>
+                    </h5>
+                    <h5>Price: ${product.price}</h5>
+                  </Card.Text>
+                  <Card.Text>
+                    <h5>
+                      Availability:
+                      {product.quantity > 0 ? " in Stock" : "out of stock"}{" "}
+                    </h5>
+                  </Card.Text>
+                  <Button variant="primary">Manage Product</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </div>
+        ))}
+      </Row>
     </div>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-
+import banner from "../Header/landing_slide-bg.jpg";
 const Header = () => {
   return (
     <div>
       <Navbar bg="primary" variant="dark" expand="lg">
         <Container>
           <Navbar.Brand href="/home">Navbar</Navbar.Brand>
-          <Navbar.Offcanvas></Navbar.Offcanvas>
+          <Navbar.Toggle></Navbar.Toggle>
           <Navbar.Collapse>
             <Nav className="me-auto">
               <Nav.Link href="/home">Home</Nav.Link>
@@ -17,6 +17,7 @@ const Header = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      <img src={banner} className="w-100" alt="" />
     </div>
   );
 };
