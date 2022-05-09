f3c6ddd66292427b8f57294d1347db45e4777065
Emran12 <emran6452@gmail.com>
2022-05-09 11:30:39 +0600

    product has been loaded from mongodb successfully

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,17 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 const Header = () => {
   return (
     <div>
-      <Navbar bg="primary" variant="dark">
+      <Navbar bg="primary" variant="dark" expand="lg">
         <Container>
           <Navbar.Brand href="/home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/products">Products</Nav.Link>
-            <Nav.Link href="/blog">Blog</Nav.Link>
-          </Nav>
+          <Navbar.Offcanvas></Navbar.Offcanvas>
+          <Navbar.Collapse>
+            <Nav className="me-auto">
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link href="/products">Products</Nav.Link>
+              <Nav.Link href="/blog">Blog</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
